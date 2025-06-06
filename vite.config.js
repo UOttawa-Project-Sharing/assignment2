3434544299fae0647c9684cafcc1ac9e9fb348fe
Tristan Robichaud <53322384+toxoscorp@users.noreply.github.com>
2025-06-06 16:27:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/assignment2/',
+  base: '/seg3525-assignment2/',
 })
