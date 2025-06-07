0cfc051828316ea2b76d5b2bf633b3075512796a
Tristan Robichaud <contact@toxoscorp.com>
2025-06-06 23:30:41 -0400

    feat(nav): item right

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ function NavigatorBar() {
             <Navbar.Brand as={Link} to="/">GymJim</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
+                <Nav className="ms-auto">
                     <Nav.Link as={Link} to="/">Home</Nav.Link>
                     <NavDropdown renderMenuOnMount={true}
                                  title={<Link style={{color: 'inherit', textDecoration: 'none'}} to="/programs"
